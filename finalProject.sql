94db224bce0e2972e474ebeac84296a4710cfb64
George <georgec.perez@gmail.com>
2015-12-14 14:57:31 -0500

    New project finished

--- finalProject.sql
+++ finalProject.sql
@@ -53,27 +53,12 @@ CREATE TABLE Itemized_orders_b
      REFERENCES Donuts_b(donutID),
      quantity int);
      
-CREATE TABLE Orders_c
-     (donutOrderID int AUTO_INCREMENT,
-      PRIMARY KEY (donutOrderID),
-      dateOfSale DATE,
-      specialNotes varchar(150));
-
 CREATE TABLE Donuts_c
      (donutID int AUTO_INCREMENT,
      PRIMARY KEY (donutID),
      donutName varchar(20),
      donutDescription varchar(100),
      unitPrice decimal);
-
-CREATE TABLE Itemized_orders_c
-     (donutOrderID int,
-     FOREIGN KEY (donutOrderID) 
-     REFERENCES Orders_c(donutOrderID),
-     donutID int,
-     FOREIGN KEY (donutID)
-     REFERENCES Donuts_c(donutID),
-     quantity int);
 
 CREATE TABLE Customers_c
     (customerID int,
@@ -88,3 +73,60 @@ CREATE TABLE Customers_c
      homePhone varchar(15),
      mobile varchar(15),
      otherPhone varchar(15));
+
+CREATE TABLE Orders_c
+     (donutOrderID int AUTO_INCREMENT,
+      PRIMARY KEY (donutOrderID),
+      customerID int,
+      FOREIGN KEY (customerID)
+      REFERENCES Customers_c(customerID),
+      dateOfSale DATE,
+      specialNotes varchar(150));
+
+CREATE TABLE Itemized_orders_c
+     (donutOrderID int,
+     FOREIGN KEY (donutOrderID) 
+     REFERENCES Orders_c(donutOrderID),
+     donutID int,
+     FOREIGN KEY (donutID)
+     REFERENCES Donuts_c(donutID),
+     PRIMARY KEY (donutOrderID, donutID),
+     quantity int);
+     
+CREATE VIEW CUSTOMERS_VIEW AS
+SELECT CONCAT(firstName, ' ', lastName) AS 'Name', 
+  customerID, address, apt, city, state, zip, homePhone, mobile, otherPhone
+FROM Customers_c;
+
+CREATE INDEX DONUTS_INDEX 
+ON Donuts_c(donutName);
+
+INSERT INTO Customers_c
+VALUES(1,
+       'Bob',
+       'Vance',
+       '123 Street Rd',
+       'd2',
+       'Scranton',
+       'PA',
+       12345,
+       '1234567890',
+       '2345678901',
+       '3456789012');
+
+INSERT INTO Donuts_c
+VALUES(1,
+       'Great Divider',
+       'Covered in chocolate and sprinkles, destined to divide family and friends over who gets to eat it',
+       1.99);
+
+INSERT Orders_c
+VALUES(1,
+       1,
+       date(20151207),
+       "none");
+
+INSERT Itemized_orders_c
+VALUES(1,
+      1,
+      10);
